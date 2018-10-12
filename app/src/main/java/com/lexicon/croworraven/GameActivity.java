1524cc4813a1d0575227df71a76fc05097094948
ajbtech <alexjburtness@gmail.com>
2018-10-12 08:22:53 -0400

    V0.1.0 Basic CardView functionality
    Added code based on: https://blog.mindorks.com/android-tinder-swipe-view-example-3eca9b0d4794
    
    Card swiping works. Need to add Raven/Crow labels next

--- app/src/main/java/com/lexicon/croworraven/GameActivity.java
+++ app/src/main/java/com/lexicon/croworraven/GameActivity.java
@@ -1,13 +1,53 @@
 package com.lexicon.croworraven;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.mindorks.placeholderview.SwipeDecor;
+import com.mindorks.placeholderview.SwipePlaceHolderView;
 
 public class GameActivity extends AppCompatActivity {
+
+    private SwipePlaceHolderView mSwipeView;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        mSwipeView = findViewById(R.id.swipeView);
+        mContext = getApplicationContext();
+
+        mSwipeView.getBuilder()
+                .setDisplayViewCount(3)
+                .setSwipeDecor(new SwipeDecor()
+                        .setPaddingTop(20)
+                        .setRelativeScale(0.01f)
+                        .setSwipeInMsgLayoutId(R.layout.bird_card_swipe_crow_correct)
+                        .setSwipeOutMsgLayoutId(R.layout.bird_card_swipe_crow_false));
+
+
+        for(Profile profile : Utils.loadProfiles(this.getApplicationContext())){
+            mSwipeView.addView(new BirdCard(mContext, profile, mSwipeView));
+        }
+
+        findViewById(R.id.rejectBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSwipeView.doSwipe(false);
+            }
+        });
+
+        findViewById(R.id.acceptBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSwipeView.doSwipe(true);
+            }
+        });
+
+
     }
 }
---
+++ app/src/test/java/com/lexicon/croworraven/MainActivityTest.java
@@ -0,0 +1,29 @@
+package com.lexicon.croworraven;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class MainActivityTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testInitializeViews(){
+
+    }
+
+    @Test
+    public void testInitializeListOfImages(){
+
+    }
+
+}
